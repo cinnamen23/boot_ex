4b237bd72accb42c3528bba2a830853a856db48e
choi-seunggyu <choi-seunggyu@DESKTOP-JHHKK9J>
2017-05-16 10:18:26 +0900

    QueryMethod Pageable

--- src/main/java/org/zerock/persistence/BoardRepository.java
+++ src/main/java/org/zerock/persistence/BoardRepository.java
@@ -1,8 +1,22 @@
 package org.zerock.persistence;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.zerock.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
 
+	
+	public Board findByBno(Long bno); //앞에 bno 틀리면 못시행 
+	
+	
+	public Board getByTitle(String title);
+	
+	
+	public List<Board> findByTitleContaining(String keyword,Pageable pagebale);//와 정렬이 안되 있었는데  그녕 붙여버리면 되버리네 ....무조건 페이지어블은 마지감 파라미터로 넣어여  
+	
+	
+	
 }
--- src/test/java/org/zerock/B2ApplicationTests.java
+++ src/test/java/org/zerock/B2ApplicationTests.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zerock.domain.Board;
 import org.zerock.persistence.BoardRepository;
@@ -20,6 +23,35 @@ public class B2ApplicationTests {
 
 	@Autowired
 	BoardRepository repo;
+	
+	
+	@Test
+	public void testFind1(){
+		Board board= repo.findByBno(200L);
+		
+		log.info(""+board);
+	}
+	
+	@Test
+	public void testFind2(){
+		Board board= repo.getByTitle("Title....196");
+		
+		log.info(""+board);
+	}
+	
+	@Test
+	public void testSearchAndPage(){
+		
+		Pageable page = new PageRequest(0, 20,Sort.Direction.DESC,"bno");  //정렬해서 뽑아주는거 
+		
+		List<Board> list= repo.findByTitleContaining("15",page);  //뒤에 L 쓰면  Long형으로 캐스팅됨  
+		
+		list.forEach(board->log.info(""+board));
+		
+	}
+	
+	
+	
 	
 	@Test
 	public void contextLoads() {
